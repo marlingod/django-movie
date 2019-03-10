d5b61bbed45fd25fce6c63ef7dc52751349cb069
malingod <malingod@yahoo.fr>
2019-03-10 05:49:51 +0000

    first commit

---
+++ core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from django.views.generic import (ListView, DetailView)
+from core.models import Movie
+
+class MovieList(ListView):
+    model = Movie
+
+class MovieDetail(DetailView):
+    model = Movie
