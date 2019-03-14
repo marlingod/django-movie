09fa2032b7d3f73db2a76a9d0a9b58b9a667ee66
malingod <malingod@yahoo.fr>
2019-03-14 04:43:00 +0000

    adding users app and voters

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'user',
     'core',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -129,3 +130,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL ='core:MovieList'
--- core/models.py
+++ core/models.py
@@ -1,4 +1,54 @@
 from django.db import models
+from django.conf import settings
+
+
+class PersonManager(models.Manager):
+    def all_with_prefetch_movies(self):
+        qs = self.get_queryset()
+        return qs.prefetch_related(
+            'directed',
+            'writing_credits',
+            'role_set__movie')
+
+
+class Person(models.Model):
+    first_name = models.CharField(
+        max_length=140)
+    last_name = models.CharField(
+        max_length=140)
+    born = models.DateField()
+    died = models.DateField(null=True,
+                            blank=True)
+
+    objects = PersonManager()
+
+    class Meta:
+        ordering = (
+            'last_name', 'first_name')
+
+    def __str__(self):
+        if self.died:
+            return '{}, {} ({}-{})'.format(
+                self.last_name,
+                self.first_name,
+                self.born,
+                self.died)
+        return '{}, {} ({})'.format(
+                self.last_name,
+                self.first_name,
+                self.born)
+
+
+class MovieManager(models.Manager):
+
+    def all_with_related_persons(self):
+        qs = self.get_queryset()
+        qs = qs.select_related(
+            'director')
+        qs = qs.prefetch_related(
+            'writers', 'actors')
+        return qs
+
 
 class Movie(models.Model):
     NOT_RATED = 0
@@ -26,6 +76,23 @@ class Movie(models.Model):
         models.PositiveIntegerField()
     website = models.URLField(
         blank=True)
+    director = models.ForeignKey(
+        to='Person',
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='directed',
+        blank=True)
+    writers = models.ManyToManyField(
+        to='Person',
+        related_name='writing_credits',
+        blank=True)
+    actors = models.ManyToManyField(
+        to='Person',
+        through='Role',
+        related_name='acting_credits',
+        blank=True)
+
+    objects = MovieManager()
 
     class Meta:
         ordering = ('-year', 'title')
@@ -33,4 +100,58 @@ class Movie(models.Model):
     def __str__(self):
         return '{} ({})'.format(
             self.title, self.year)
+
+
+class Role(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.DO_NOTHING)
+    person = models.ForeignKey(Person, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=140)
+
+    def __str__(self):
+        return "{} {} {}".format(self.movie_id, self.person_id, self.name)
+
+    class Meta:
+        unique_together = ('movie',
+                           'person',
+                           'name')
+
+class VoteManager(models.Manager):
+
+    def get_vote_or_unsaved_blank_vote(self, movie, user):
+        try:
+            return Vote.objects.get(
+                movie=movie,
+                user=user)
+        except Vote.DoesNotExist:
+            return Vote(
+                movie=movie,
+                user=user)
+
+
+class Vote(models.Model):
+    UP = 1
+    DOWN = -1
+    VALUE_CHOICES = (
+        (UP, "👍",),
+        (DOWN, "👎",),
+    )
+
+    value = models.SmallIntegerField(
+        choices=VALUE_CHOICES,
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE,
+    )
+    voted_on = models.DateTimeField(
+        auto_now=True
+    )
+
+    objects = VoteManager()
+    class Meta:
+        unique_together = ('user', 'movie')
 
